bde6f4691c4df39bc14c4a12c6e35a2145d35659
fuck them <ruiazevedo1666@gmail.com>
2019-06-04 18:24:47 +0100

    2º aula

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+
+def gravar(v1, v2):
+    import sqlite3
+    ficheiro = sqlite3.connect('db/Utilizador.db')
+    db = ficheiro.cursor()
+    db.execute('CREATE TABLE IF NOT EXISTS usr (usr text, pwd text)')
+    db.execute("INSERT INTO usr VALUES (?, ?)", (v1, v2))
+    ficheiro.commit()
+    ficheiro.close()
+    return
+
+def alterar(v1, v2):
+    import sqlite3
+    ficheiro = sqlite3.connect('db/Utilizador.db')
+    db = ficheiro.cursor()
+    db.execute("UPDATE usr SET pwd = ? WHERE usr = ?",(v2, v1))
+    ficheiro.commit()
+    ficheiro.close()
+    return
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    erro = None
+    if request.method == "POST":
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
+        v3 = request.form['cpwd']
+        if v2 != v3:
+            erro = 'A palavra-passe não coincide.'
+        else:
+            alterar(v1, v2)
+    return render_template('newpass.html', erro=erro)
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
-@app.route('/registro')
-def registro():
-    return render_template('registro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
